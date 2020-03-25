3d90a5b88be9db8e1090e6e87545754cebc2c3e4
emanuelwanyonyi <ewanyonyi@kenet.or.ke>
2020-03-25 23:08:34 +0300

    Added todo app

--- realtimeapp/settings.py
+++ realtimeapp/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',  
     'rest_framework',
-    'corsheaders',
-    'backend',
-    'customers'
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware', 
 ]
 
 CORS_ORIGIN_ALLOW_ALL = False
